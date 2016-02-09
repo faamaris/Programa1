ebae39da7d7dec83c17fd56b15d5514b81dcde1c
SenaAdmin <SenaAdmin@ColsutecR3-PC>
2016-02-08 20:06:03 -0500

    tercera version

--- Proyecto1/src/proyecto1/Proyecto1.java
+++ Proyecto1/src/proyecto1/Proyecto1.java
@@ -1,24 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto1;
 
-/**
- *
- * @author SenaAdmin
- */
+
 public class Proyecto1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-       int a=5+5;
-       int suma=a;
-       System.out.println("SUMA  ES "+ suma);
+      
        
     }
-    
+  public void metodo(){
+      System.out.println(" esto es un metodo");
+  }  
 }
