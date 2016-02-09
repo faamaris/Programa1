7e1d4accf490b4631390bad6818101630e06988e
SenaAdmin <SenaAdmin@ColsutecR3-PC>
2016-02-08 19:59:15 -0500

    segunda version definicion de variables  y muestra de resultados

--- Proyecto1/src/proyecto1/Proyecto1.java
+++ Proyecto1/src/proyecto1/Proyecto1.java
@@ -15,7 +15,10 @@ public class Proyecto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       int a=5+5;
+       int suma=a;
+       System.out.println("SUMA  ES "+ suma);
+       
     }
     
 }
